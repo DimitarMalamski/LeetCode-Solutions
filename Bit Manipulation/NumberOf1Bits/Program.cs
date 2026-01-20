725e81992454ad39955bca2303d80800fa9efcb8
DimitarMalamski <mitemalamata@gmail.com>
2026-01-20 16:11:25 +0100

    add solution for Number of 1 Bits ( Bit Manipulation )

---
+++ Bit Manipulation/NumberOf1Bits/Program.cs
@@ -0,0 +1,24 @@
+﻿// Problem 191. Number of 1 Bits
+// Link https://leetcode.com/problems/number-of-1-bits/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 11;
+      int output = HammingWeight(n);
+      Console.WriteLine(output);
+   }
+   static int HammingWeight(int n)
+   {
+      int count = 0;
+
+      while (n > 0)
+      {
+         count += (int)(n & 1);
+         n >>= 1;
+      }
+
+      return count;
+   }
+}
