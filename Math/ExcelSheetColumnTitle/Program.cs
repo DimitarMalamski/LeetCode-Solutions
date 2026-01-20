b05ae3571baf20ba279e9860d2775bb28e92500d
DimitarMalamski <mitemalamata@gmail.com>
2026-01-20 15:47:00 +0100

    add solution for Excel Sheet Column Title ( Math )

---
+++ Math/ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,29 @@
+﻿// Problem 168. Excel Sheet Column Title
+// Link https://leetcode.com/problems/excel-sheet-column-title/description/
+// Difficulty: Easy
+
+using System.Text;
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int columnNumber = 28;
+      string output = ConvertToTitle(columnNumber);
+      Console.WriteLine(output);
+   }
+   static string ConvertToTitle(int columnNumber)
+   {
+      StringBuilder result = new StringBuilder();
+
+      while (columnNumber > 0)
+      {
+         columnNumber--;
+         int remainder = columnNumber % 26;
+         char letter = (char)('A' + remainder);
+         result.Append(letter);
+         columnNumber /= 26;
+      }
+
+      return new string(result.ToString().Reverse().ToArray());
+   }
+}
