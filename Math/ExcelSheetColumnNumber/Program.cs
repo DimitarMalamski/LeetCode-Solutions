0c92b2cd9f7ec4b418dd5b7036ab31cf415a3c24
DimitarMalamski <mitemalamata@gmail.com>
2026-01-20 15:55:21 +0100

    add solution for Excel Sheet Column Number ( Math )

---
+++ Math/ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// Problem 171. Excel Sheet Column Number
+// Link https://leetcode.com/problems/excel-sheet-column-number/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string columnTitle = "ZY";
+      int output = TitleToNumber(columnTitle);
+      Console.WriteLine(output);
+   }
+   static int TitleToNumber(string columnTitle)
+   {
+      int result = 0;
+
+      foreach (char c in columnTitle)
+      {
+         int value = c - 'A' + 1;
+         result = result * 26 + value;
+      }
+
+      return result;
+   }
+}
