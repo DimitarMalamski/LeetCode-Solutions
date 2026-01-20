7250567fff59078bc469e9d3a33c4a2a116667e1
DimitarMalamski <mitemalamata@gmail.com>
2026-01-20 16:05:22 +0100

    add solution for Reverse Bits ( Bit Manipulation )

---
+++ Bit Manipulation/ReverseBits/Program.cs
@@ -0,0 +1,25 @@
+﻿// Problem 190. Reverse Bits
+// Link https://leetcode.com/problems/reverse-bits/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 43261596;
+      int output = ReverseBits(n);
+      Console.WriteLine(output);
+   }
+   static int ReverseBits(int n)
+   {
+      int result = 0;
+
+      for (int i = 0; i < 32; i++)
+      {
+         int bit = n & 1;
+         result = (result << 1) | bit;
+         n >>= 1;
+      }
+
+      return result;
+   }
+}
