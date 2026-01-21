55c6024d39ced787c9dc4c039bd0114759ea31ad
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 15:44:02 +0100

    add solution for Counting Bits ( Bit Manipulation )

---
+++ Bit Manipulation/CountingBits/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem 338. Counting Bits
+// Link https://leetcode.com/problems/counting-bits/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 2;
+      int[] output = CountBits(n);
+      Console.WriteLine(string.Join(",", output));
+   }
+   public static int[] CountBits(int n)
+   {
+      int[] result = new int[n + 1];
+
+      for (int i = 1; i <= n; i++)
+      {
+         result[i] = result[i >> 1] + (i & 1);
+      }
+
+      return result;
+   }
+}
