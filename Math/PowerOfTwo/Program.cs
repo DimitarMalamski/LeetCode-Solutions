40cb432b1513ee594948f729ea65012abf6a622a
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 13:48:51 +0100

    add solution for Power of Two ( Math )

---
+++ Math/PowerOfTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿// Problem 231. Power of Two
+// Link https://leetcode.com/problems/power-of-two/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 0;
+      bool output = IsPowerOfTwo(n);
+      Console.WriteLine(output);
+   }
+   public static bool IsPowerOfTwo(int n)
+   {
+      return n > 0 && (n & (n - 1)) == 0;  
+   }
+}
