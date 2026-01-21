2f6ef4c5f811c5a1381d2bf729049dfa6c8606b0
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 15:29:21 +0100

    add solution for Power of Three ( Math )

---
+++ Math/PowerOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿// Problem 326. Power of Three
+// Link https://leetcode.com/problems/power-of-three/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 27;
+      bool output = IsPowerOfThree(n);
+      Console.WriteLine(output);
+   }
+   public static bool IsPowerOfThree(int n)
+   {
+      return n > 0 && 1162261467 % n == 0;
+   }
+}
