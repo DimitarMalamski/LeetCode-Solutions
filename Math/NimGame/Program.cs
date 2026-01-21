0bbafed2ea38df7ee188d1cad95d9011aa35df1a
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 15:04:31 +0100

    add solution for Nim Game ( Math )

---
+++ Math/NimGame/Program.cs
@@ -0,0 +1,16 @@
+﻿// Problem 292. Nim Game
+// Link https://leetcode.com/problems/nim-game/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 4;
+      bool output = CanWinNim(n);
+      Console.WriteLine(output);
+   }
+   public static bool CanWinNim(int n)
+   {
+      return n % 4 != 0;
+   }
+}
