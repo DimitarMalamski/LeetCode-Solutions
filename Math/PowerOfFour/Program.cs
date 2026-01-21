182b0b888d72c5ad6abd5cc3181a1401a0d4d617
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 15:50:18 +0100

    add solution for Power of Four ( Math )

---
+++ Math/PowerOfFour/Program.cs
@@ -0,0 +1,22 @@
+﻿// Problem 342. Power of Four
+// Link https://leetcode.com/problems/power-of-four/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 15;
+      bool output = IsPowerOfFour(n);
+      Console.WriteLine(output);
+   }
+   public static bool IsPowerOfFour(int n)
+   {
+      if (n <= 0) return false;
+      while (n % 4 == 0)
+      {
+         n /= 4;
+      }
+
+      return n == 1;
+   }
+}
