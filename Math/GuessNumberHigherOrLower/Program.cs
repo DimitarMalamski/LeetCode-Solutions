98e95322be4658920a3883b6ee2884d58d495c1e
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 16:50:31 +0100

    add solution for Guess Number Higher or Lower ( Math )

---
+++ Math/GuessNumberHigherOrLower/Program.cs
@@ -0,0 +1,10 @@
+﻿// Problem 374. Guess Number Higher or Lower
+// Link https://leetcode.com/problems/guess-number-higher-or-lower/description/
+// Difficulty: Easy
+public class Solution : GuessGame
+{
+   public int GuessNumber(int n)
+   {
+      // this is a placeholder. This taks cannot be tested here.
+   }
+}
