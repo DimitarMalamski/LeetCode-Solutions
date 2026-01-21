af3156069316c8d71833c30e4f23f45b6e5cc9b3
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 14:44:26 +0100

    add solution for First Bad Version ( Two Pointers )

---
+++ Two pointers/FirstBadVersion/Program.cs
@@ -0,0 +1,10 @@
+﻿// Problem 278. First Bad Version
+// Link https://leetcode.com/problems/first-bad-version/description/
+// Difficulty: Easy
+class Solution : VersionControl
+{
+   public int FirstBadVersion(int n)
+   {
+      // This is a placeholder, the same as in Leedcode. This task cannot be tested here.
+   }
+}
