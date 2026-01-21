b34419dec1e41221cad27d03fd637c156cc4fbb6
DimitarMalamski <mitemalamata@gmail.com>
2026-01-21 16:44:44 +0100

    add solution for Valid Perfect Square ( Math )

---
+++ Math/ValidPerfectSquare/Program.cs
@@ -0,0 +1,37 @@
+﻿// Problem 367. Valid Perfect Square
+// Link https://leetcode.com/problems/valid-perfect-square/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int num = 14;
+      bool output = IsPerfectSquare(num);
+      Console.WriteLine(output);
+   }
+   public static bool IsPerfectSquare(int num)
+   {
+      long left = 1, right = num;
+
+      while (left <= right)
+      {
+         long mid = left + (right - left) / 2;
+         long square = mid * mid;
+
+         if (square == num)
+         {
+            return true;
+         }
+         else if (square < num)
+         {
+            left = mid + 1;
+         }
+         else
+         {
+            right = mid - 1;
+         }
+      }
+
+      return false;
+   }
+}
