512a5f7b8b37d7250accaba97b2030ae50089bdc
DimitarMalamski <mitemalamata@gmail.com>
2026-01-27 12:25:29 +0100

    add solution for Arranging Coins ( Math )

---
+++ Math/ArrangingCoins/Program.cs
@@ -0,0 +1,16 @@
+﻿// Problem 441. Arranging Coins
+// Link https://leetcode.com/problems/arranging-coins/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int n = 5;
+    int output = ArrangeCoins(n);
+    Console.WriteLine(output);
+  }
+  public static int ArrangeCoins(int n)
+  {
+    return (int)((Math.Sqrt(1 + 8L * n) - 1) / 2);
+  }
+}
