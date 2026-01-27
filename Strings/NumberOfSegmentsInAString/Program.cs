6edb5f4fa1750e0807cb0e9568f71809714c3338
DimitarMalamski <mitemalamata@gmail.com>
2026-01-27 12:18:07 +0100

    add solution for Number of Segments in a String ( Strings )

---
+++ Strings/NumberOfSegmentsInAString/Program.cs
@@ -0,0 +1,26 @@
+﻿// Problem 434. Number of Segments in a String
+// Link https://leetcode.com/problems/number-of-segments-in-a-string/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "Hello, my name is John";
+    int output = CountSegments(s);
+    Console.WriteLine(output);
+  }
+  public static int CountSegments(string s)
+  {
+    int count = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+      if (s[i] != ' ' && (i == 0 || s[i - 1] == ' '))
+      {
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
