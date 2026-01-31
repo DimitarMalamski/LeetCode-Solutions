0aa1d3e767f0a4d03f519d03b14ae7bfb1ab49bc
DimitarMalamski <mitemalamata@gmail.com>
2026-01-31 10:49:34 +0100

    add solution for Number Complement ( Bit Manipulation )

---
+++ Bit Manipulation/NumberComplement/Program.cs
@@ -0,0 +1,24 @@
+﻿// Problem 476. Number Complement
+// Link https://leetcode.com/problems/number-complement/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int num = 5;
+    int output = FindComplement(num);
+    Console.WriteLine(output);
+  }
+  public static int FindComplement(int num)
+  {
+    long mask = 1;
+
+    while (mask <= num)
+    {
+      mask <<= 1;
+    }
+
+    return (int)((mask - 1) ^ num);
+  }
+}
+
