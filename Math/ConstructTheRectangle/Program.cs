304048bbcbc11fd15047a610a72e20e2f428780c
DimitarMalamski <mitemalamata@gmail.com>
2026-02-02 17:59:55 +0100

    add solution for Construct the Rectangle ( Math )

---
+++ Math/ConstructTheRectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿// Problem 492. Construct the Rectangle
+// Link https://leetcode.com/problems/construct-the-rectangle/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int area = 4;
+    int[] output = ConstructRectangle(area);
+    Console.WriteLine(string.Join(" ", output));
+  }
+  public static int[] ConstructRectangle(int area)
+  {
+    int w = (int)Math.Sqrt(area);
+
+    while (area % w != 0)
+    {
+      w--;
+    }
+
+    int l = area / w;
+    return [l, w];
+  }
+}
