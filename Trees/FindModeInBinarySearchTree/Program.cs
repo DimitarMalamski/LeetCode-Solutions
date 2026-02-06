1760302b60424c85e5881ffb31547ba6f5c148d8
DimitarMalamski <mitemalamata@gmail.com>
2026-02-06 15:04:46 +0100

    add solution for Find Mode in Binary Search Tree ( Trees )

---
+++ Trees/FindModeInBinarySearchTree/Program.cs
@@ -0,0 +1,70 @@
+﻿// Problem 501. Find Mode in Binary Search Tree
+// Link https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    Solution sol = new Solution();
+
+    TreeNode root = new TreeNode(1);
+    root.right = new TreeNode(2);
+
+    root.right.left = new TreeNode(2);
+
+    int[] output = sol.FindMode(root);
+    Console.WriteLine(string.Join(",", output));
+  }
+  public int[] FindMode(TreeNode root)
+  {
+    int? prev = null;
+    int count = 0;
+    int maxCount = 0;
+    List<int> nodes = new List<int>();
+
+    void InOrder(TreeNode node)
+    {
+      if (node == null) return;
+
+      InOrder(node.left);
+
+      if (prev == node.val)
+      {
+        count++;
+      }
+      else
+      {
+        count = 1;
+        prev = node.val;
+      }
+
+      if (count > maxCount)
+      {
+        maxCount = count;
+        nodes.Clear();
+        nodes.Add(node.val);
+      }
+      else if (count == maxCount)
+      {
+        nodes.Add(node.val);
+      }
+
+      InOrder(node.right);
+    }
+
+    InOrder(root);
+    return nodes.ToArray();
+  }
+  public class TreeNode
+  {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
