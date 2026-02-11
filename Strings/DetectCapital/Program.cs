23506dfa52d4f4b14a9f8f66e6652be327836f35
DimitarMalamski <mitemalamata@gmail.com>
2026-02-11 16:20:24 +0100

    add solution for Detect Capital ( Strings )

---
+++ Strings/DetectCapital/Program.cs
@@ -0,0 +1,18 @@
+﻿// Problem 520. Detect Capital
+// Link https://leetcode.com/problems/detect-capital/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string word = "FlaG";
+    bool output = DetectCapitalUse(word);
+    Console.WriteLine(output);
+  }
+  public static bool DetectCapitalUse(string word)
+  {
+    return word == word.ToUpper() ||
+          word == word.ToLower() ||
+          word == char.ToUpper(word[0]) + word.Substring(1).ToLower();
+  }
+}
