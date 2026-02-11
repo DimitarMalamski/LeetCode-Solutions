800962163c8e2ea2ccb45c5317f91612a03df658
DimitarMalamski <mitemalamata@gmail.com>
2026-02-11 16:09:44 +0100

    add solution for Fibonacci Number ( Math )

---
+++ Math/FibonacciNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// Problem 509. Fibonacci Number
+// Link https://leetcode.com/problems/fibonacci-number/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int n = 4;
+    int output = Fib(n);
+    Console.WriteLine(output);
+  }
+  public static int Fib(int n)
+  {
+    if (n < 2) return n;
+
+    return Fib(n - 1) + Fib(n - 2);
+  }
+}
