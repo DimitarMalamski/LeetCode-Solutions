b811e86e3d7f8dd45fa231cf9eb54c938d24c99e
DimitarMalamski <mitemalamata@gmail.com>
2026-02-12 18:34:11 +0100

    add solution for Longest Uncommon Subsequence I ( Strings )

---
+++ Strings/LongestUncommonSubsequenceI/Program.cs
@@ -0,0 +1,16 @@
+﻿// Problem 521. Longest Uncommon Subsequence I
+// Link https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string a = "aba", b = "cdc";
+    int output = FindLUSlength(a, b);
+    Console.WriteLine(output);
+  }
+  public static int FindLUSlength(string a, string b)
+  {
+    return a == b ? -1 : Math.Max(a.Length, b.Length);
+  }
+}
