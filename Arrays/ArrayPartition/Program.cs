3c0cac5c4aefd642b64dca03fc7d5d96b062548a
DimitarMalamski <mitemalamata@gmail.com>
2026-02-23 14:36:07 +0100

    add solution for Array Partition ( Arrays )

---
+++ Arrays/ArrayPartition/Program.cs
@@ -0,0 +1,25 @@
+﻿// Problem 561. Array Partition
+// Link https://leetcode.com/problems/array-partition/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] nums = { 1, 4, 3, 2 };
+    int output = ArrayPairSum(nums);
+    Console.WriteLine(output);
+  }
+  public static int ArrayPairSum(int[] nums)
+  {
+    Array.Sort(nums);
+
+    int sum = 0;
+
+    for (int i = 0; i < nums.Length; i += 2)
+    {
+      sum += nums[i];
+    }
+
+    return sum;
+  }
+}
