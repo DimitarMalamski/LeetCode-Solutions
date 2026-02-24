d015b788f441fe354e67f4d2317f90bb61a6c7dd
DimitarMalamski <mitemalamata@gmail.com>
2026-02-24 13:27:03 +0100

    add solution for Distribute Candies ( Arrays )

---
+++ Arrays/DistributeCandies/Program.cs
@@ -0,0 +1,19 @@
+﻿// Problem 575. Distribute Candies
+// Link https://leetcode.com/problems/distribute-candies/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] candyType = { 1, 1, 2, 2, 3, 3 };
+    int output = DistributeCandies(candyType);
+    Console.WriteLine(output);
+  }
+  public static int DistributeCandies(int[] candyType)
+  {
+    HashSet<int> uniqueTypes = new HashSet<int>(candyType);
+    int maxAllowed = candyType.Length / 2;
+
+    return Math.Min(uniqueTypes.Count, maxAllowed);
+  }
+}
