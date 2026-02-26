4ea4642de8fb5ed85e55a0a55c63ea5a0478ad65
DimitarMalamski <mitemalamata@gmail.com>
2026-02-26 17:16:00 +0100

    add solution for N-ary Tree Postorder Traversal ( Trees )

---
+++ Trees/N-aryTreePostorderTraversal/Program.cs
@@ -0,0 +1,83 @@
+﻿// Problem 590. N-ary Tree Postorder Traversal
+// Link https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int?[] input = { 1,null,3,2,4,null,5,6 };
+
+    Node root = BuildTree(input);
+
+    var output = Postorder(root);
+
+    Console.WriteLine(string.Join(", ", output));
+  }
+  public static IList<int> Postorder(Node root)
+  {
+    List<int> result = new List<int>();
+    DFS(root, result);
+    return result;
+  }
+  private static void DFS(Node node, List<int> result)
+  {
+    if (node == null) return;
+
+    foreach (var child in node.children)
+    {
+      DFS(child, result);
+    }
+
+    result.Add(node.val);
+  }
+  public static Node? BuildTree(int?[] data)
+  {
+    if (data == null || data.Length == 0)
+      return null;
+
+    Node root = new Node(data[0]!.Value);
+    root.children = new List<Node>();
+
+    Queue<Node> queue = new Queue<Node>();
+    queue.Enqueue(root);
+
+    int i = 1;
+
+    while (queue.Count > 0 && i < data.Length)
+    {
+      Node parent = queue.Dequeue();
+
+      if (data[i] == null)
+      {
+        i++;
+      }
+
+      while (i < data.Length && data[i] != null)
+      {
+        Node child = new Node(data[i]!.Value);
+        child.children = new List<Node>();
+
+        parent.children.Add(child);
+        queue.Enqueue(child);
+        i++;
+      }
+    }
+
+    return root;
+  }
+  public class Node
+  {
+    public int val;
+    public IList<Node> children;
+    public Node() { }
+    public Node(int _val)
+    {
+      val = _val;
+    }
+    public Node(int _val, IList<Node> _children)
+    {
+      val = _val;
+      children = _children;
+    }
+  }
+}
