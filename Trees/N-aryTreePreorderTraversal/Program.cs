73327903da26371df1681a053c4b09f4c350e15d
DimitarMalamski <mitemalamata@gmail.com>
2026-02-26 17:05:06 +0100

    add solution for N-ary Tree Preorder Traversal ( Trees )

---
+++ Trees/N-aryTreePreorderTraversal/Program.cs
@@ -0,0 +1,90 @@
+﻿// Problem 589. N-ary Tree Preorder Traversal
+// Link https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int?[] input = { 1, null, 3, 2, 4, null, 5, 6 };
+
+    Node root = BuildTree(input);
+
+    var output = Preorder(root);
+
+    Console.WriteLine(string.Join(", ", output));
+  }
+  public static IList<int> Preorder(Node root)
+  {
+    List<int> result = new List<int>();
+    if (root == null) return result;
+
+    Stack<Node> stack = new Stack<Node>();
+    stack.Push(root);
+
+    while (stack.Count > 0)
+    {
+      Node node = stack.Pop();
+      result.Add(node.val);
+
+      if (node.children != null)
+      {
+        for (int j = node.children.Count - 1; j >= 0; j--)
+        {
+          stack.Push(node.children[j]);
+        } 
+      }
+    }
+
+    return result;
+  }
+  public static Node? BuildTree(int?[] data)
+  {
+    if (data == null || data.Length == 0)
+      return null;
+
+    Node root = new Node(data[0]!.Value);
+    root.children = new List<Node>();
+
+    Queue<Node> queue = new Queue<Node>();
+    queue.Enqueue(root);
+
+    int i = 1;
+
+    while (queue.Count > 0 && i < data.Length)
+    {
+      Node parent = queue.Dequeue();
+      
+      if (data[i] == null)
+      {
+        i++;
+      }
+
+      while (i < data.Length && data[i] != null)
+      {
+        Node child = new Node(data[i]!.Value);
+        child.children = new List<Node>();
+
+        parent.children.Add(child);
+        queue.Enqueue(child);
+        i++;
+      }
+    }
+
+    return root;
+  }
+  public class Node
+  {
+    public int val;
+    public IList<Node> children;
+    public Node() { }
+    public Node(int _val)
+    {
+      val = _val;
+    }
+    public Node(int _val, IList<Node> _children)
+    {
+      val = _val;
+      children = _children;
+    }
+  }
+}
