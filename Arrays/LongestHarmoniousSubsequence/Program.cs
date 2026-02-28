01fcd7676524ccdad258ebd441744edf78896b2b
DimitarMalamski <mitemalamata@gmail.com>
2026-02-28 10:44:21 +0100

    add solution for Longest Harmonious Subsequence ( Arrays )

---
+++ Arrays/LongestHarmoniousSubsequence/Program.cs
@@ -0,0 +1,43 @@
+﻿// Problem 594. Longest Harmonious Subsequence
+// Link https://leetcode.com/problems/longest-harmonious-subsequence/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] nums = { 1, 3, 2, 2, 5, 2, 3, 7 };
+    int output = FindLHS(nums);
+    Console.WriteLine(output);
+  }
+  public static int FindLHS(int[] nums)
+  {
+    Dictionary<int, int> freq = new Dictionary<int, int>();
+
+    foreach (int num in nums)
+    {
+      if (freq.ContainsKey(num))
+      {
+        freq[num]++;
+      }
+      else
+      {
+        freq[num] = 1;
+      }
+    }
+
+    int longest = 0;
+
+    foreach (var pair in freq)
+    {
+      int num = pair.Key;
+
+      if (freq.ContainsKey(num + 1))
+      {
+        int length = freq[num] + freq[num + 1];
+        longest = Math.Max(longest, length);
+      }
+    }
+
+    return longest;
+  }
+}
