c5b6f1c9173dff2501fd18cfb0c5c44d018e949e
DimitarMalamski <mitemalamata@gmail.com>
2026-03-04 10:15:54 +0100

    add solution for Maximum Product of Three Numbers ( Arrays )

---
+++ Arrays/MaximumProductOfThreeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem 628. Maximum Product of Three Numbers
+// Link https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] nums = { 1, 2, 3, 4 };
+    int output = MaximumProduct(nums);
+    Console.WriteLine(output);
+  }
+  public static int MaximumProduct(int[] nums)
+  {
+    Array.Sort(nums);
+
+    int n = nums.Length;
+
+    int optionOne = nums[n - 1] * nums[n - 2] * nums[n - 3];
+    int optionTwo = nums[0] * nums[1] * nums[n - 1];
+
+    return Math.Max(optionOne, optionTwo);
+  }
+}
