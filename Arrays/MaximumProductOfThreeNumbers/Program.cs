9a2a2d02d964b03dee4d17251c5dc18cb9aff119
DimitarMalamski <mitemalamata@gmail.com>
2026-03-04 10:19:34 +0100

    optimize solution for Maximum Product of Three Numbers ( Arrays )

--- Arrays/MaximumProductOfThreeNumbers/Program.cs
+++ Arrays/MaximumProductOfThreeNumbers/Program.cs
@@ -6,7 +6,7 @@ class Solution
   static void Main(string[] args)
   {
     int[] nums = { 1, 2, 3, 4 };
-    int output = MaximumProduct(nums);
+    int output = MaximumProductOptimized(nums);
     Console.WriteLine(output);
   }
   public static int MaximumProduct(int[] nums)
@@ -19,5 +19,41 @@ class Solution
     int optionTwo = nums[0] * nums[1] * nums[n - 1];
 
     return Math.Max(optionOne, optionTwo);
+  }
+  public static int MaximumProductOptimized(int[] nums)
+  {
+    int max1 = int.MinValue, max2 = int.MinValue, max3 = int.MinValue;
+    int min1 = int.MaxValue, min2 = int.MaxValue;
+
+    foreach (int n in nums)
+    {
+      if (n > max1)
+      {
+        max3 = max2;
+        max2 = max1;
+        max1 = n;
+      }
+      else if (n > max2)
+      {
+        max3 = max2;
+        max2 = n;
+      }
+      else if (n > max3)
+      {
+        max3 = n;
+      }
+
+      if (n < min1)
+      {
+        min2 = min1;
+        min1 = n;
+      }
+      else if (n < min2)
+      {
+        min2 = n;
+      }
+    }
+
+    return Math.Max(max1 * max2 * max3, max1 * min1 * min2);
   }
 }
