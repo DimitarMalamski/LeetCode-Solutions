da57e13e3444f7698344fec6a42d1a85a4433bbe
DimitarMalamski <mitemalamata@gmail.com>
2026-03-09 15:48:00 +0100

    add solution for Set Mismatch ( Arrays )

---
+++ Arrays/SetMismatch/Program.cs
@@ -0,0 +1,33 @@
+﻿// Problem 645. Set Mismatch
+// Link https://leetcode.com/problems/set-mismatch/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] nums = { 1, 2, 2, 4 };
+    int[] output = FindErrorNums(nums);
+    Console.WriteLine(string.Join(",", output));
+  }
+  public static int[] FindErrorNums(int[] nums)
+  {
+    int n = nums.Length;
+    int[] count = new int[n + 1];
+
+    foreach (int num in nums)
+    {
+      count[num]++;
+    }
+
+    int duplicate = -1;
+    int missing = -1;
+
+    for (int i = 1; i <= n; i++)
+    {
+      if (count[i] == 2) duplicate = i;
+      if (count[i] == 0) missing = i;
+    }
+
+    return [duplicate, missing];
+  } 
+}
