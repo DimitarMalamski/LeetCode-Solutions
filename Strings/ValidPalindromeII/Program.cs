3c07813d2d0c7d11bc428bd003bbf9e474a72e3a
DimitarMalamski <mitemalamata@gmail.com>
2026-03-11 10:28:14 +0100

    add solution for Valid Palindrome II ( Strings )

---
+++ Strings/ValidPalindromeII/Program.cs
@@ -0,0 +1,42 @@
+﻿// Problem 680. Valid Palindrome II
+// Link https://leetcode.com/problems/valid-palindrome-ii/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "aba";
+    bool output = ValidPalindrome(s);
+    Console.WriteLine(output);
+  }
+  public static bool ValidPalindrome(string s)
+  {
+    int l = 0;
+    int r = s.Length - 1;
+
+    while (l < r)
+    {
+      if (s[l] != s[r])
+      {
+        return IsPalindrome(s, l + 1, r) || IsPalindrome(s, l, r - 1);
+      }
+
+      l++;
+      r--;
+    }
+
+    return true;
+  }
+  private static bool IsPalindrome(string s, int l, int r)
+  {
+    while (l < r)
+    {
+      if (s[l] != s[r]) return false;
+
+      l++;
+      r--;
+    }
+
+    return true;
+  }
+}
