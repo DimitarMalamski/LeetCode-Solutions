2f862f95a3a0d96c267bf4eb5a50211401dd1b13
DimitarMalamski <mitemalamata@gmail.com>
2026-03-12 07:40:33 +0100

    add solution for Binary Number with Alternating Bits ( Bit Manipulation )

---
+++ Bit Manipulation/BinaryNumberWithAlternatingBits/Program.cs
@@ -0,0 +1,30 @@
+﻿// Problem 693. Binary Number with Alternating Bits
+// Link https://leetcode.com/problems/binary-number-with-alternating-bits/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int n = 5;
+    bool output = HasAlternatingBits(n);
+    Console.WriteLine(output);
+  }
+  public static bool HasAlternatingBits(int n)
+  {
+    int lastBit = n & 1;
+
+    n >>= 1;
+
+    while (n > 0)
+    {
+      int currentBit = n & 1;
+
+      if (currentBit == lastBit) return false;
+
+      lastBit = currentBit;
+      n >>= 1;
+    }
+
+    return true;
+  }
+}
