2687b1ba31f0aedd68732cbfbca3b5a501b5f801
DimitarMalamski <mitemalamata@gmail.com>
2026-03-13 10:41:50 +0100

    add solution for Count Binary Substrings ( Strings )

---
+++ Strings/CountBinarySubstrings/Program.cs
@@ -0,0 +1,31 @@
+﻿// Problem 696. Count Binary Substrings
+// Link https://leetcode.com/problems/count-binary-substrings/description/
+// Difficulty: Easy
+class Solution {
+  static void Main(string[] args) {
+    string s = "00110011";
+    int output = CountBinarySubstrings(s);
+    Console.WriteLine(output);
+  }
+  public static int CountBinarySubstrings(string s) {
+    int prevGroup = 0;
+    int currGroup = 1;
+    int result = 0;
+
+    for (int i = 1; i < s.Length; i++)
+    {
+      if (s[i] == s[i - 1]) {
+        currGroup++;
+      }
+      else {
+        result += Math.Min(prevGroup, currGroup);
+        prevGroup = currGroup;
+        currGroup = 1;
+      }
+    }
+
+    result += Math.Min(prevGroup, currGroup);
+
+    return result;
+  }
+}
