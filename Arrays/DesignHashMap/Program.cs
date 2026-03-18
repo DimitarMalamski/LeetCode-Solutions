fb694b592c2287670b1a5715e179564f0f22d5a0
DimitarMalamski <mitemalamata@gmail.com>
2026-03-18 13:03:31 +0100

    add solution for Design HashMap ( Arrays )

---
+++ Arrays/DesignHashMap/Program.cs
@@ -0,0 +1,44 @@
+﻿// Problem 706. Design HashMap
+// Link https://leetcode.com/problems/design-hashmap/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    var map = new MyHashMap();
+
+    map.Put(1, 1);
+    map.Put(2, 2);
+    Console.WriteLine(map.Get(1));
+    Console.WriteLine(map.Get(3));
+    map.Put(2, 1);
+    Console.WriteLine(map.Get(2));
+    map.Remove(2);
+    Console.WriteLine(map.Get(2));
+  }
+  public class MyHashMap
+  {
+    private int[] map;
+
+    public MyHashMap()
+    {
+      map = new int[1_000_001];
+      Array.Fill(map, -1);
+    }
+
+    public void Put(int key, int value)
+    {
+      map[key] = value;
+    }
+
+    public int Get(int key)
+    {
+      return map[key];
+    }
+
+    public void Remove(int key)
+    {
+      map[key] = -1;
+    }
+  }
+}
