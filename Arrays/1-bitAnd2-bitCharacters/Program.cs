6a139c229e171be14b57e268ae69d7a061681163
DimitarMalamski <mitemalamata@gmail.com>
2026-03-19 14:34:02 +0100

    add solution for 1-bit and 2-bit Characters ( Arrays )

---
+++ Arrays/1-bitAnd2-bitCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿// Problem 717. 1-bit and 2-bit Characters
+// Link https://leetcode.com/problems/1-bit-and-2-bit-characters/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] bits = { 1, 0, 0 };
+    bool output = IsOneBitCharacter(bits);
+    Console.WriteLine(output);
+  }
+  public static bool IsOneBitCharacter(int[] bits)
+  {
+    int i = 0;
+
+    while (i < bits.Length - 1)
+    {
+      if (bits[i] == 1)
+      {
+        i += 2;
+      }
+      else
+      {
+        i += 1;
+      }
+    }
+
+    return i == bits.Length - 1;
+  }
+}
