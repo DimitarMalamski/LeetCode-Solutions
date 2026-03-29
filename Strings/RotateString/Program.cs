ce28f4de0b69fa6b9a03bef4f309a8366cbf872a
DimitarMalamski <mitemalamata@gmail.com>
2026-03-29 20:08:58 +0200

    add solution for Rotate String ( Strings )

---
+++ Strings/RotateString/Program.cs
@@ -0,0 +1,19 @@
+﻿// Problem 796. Rotate String
+// Link https://leetcode.com/problems/rotate-string/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "abcde", goal = "cdeab";
+    bool output = RotateString(s, goal);
+    Console.WriteLine(output);
+  }
+  public static bool RotateString(string s, string goal)
+  {
+    if (s.Length != goal.Length) return false;
+
+    string doubled = s + s;
+    return doubled.Contains(goal);
+  }
+}
