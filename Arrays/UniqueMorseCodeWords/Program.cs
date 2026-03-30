28ea5129ecc636916c7be46cc66bcf6bf900826c
DimitarMalamski <mitemalamata@gmail.com>
2026-03-30 13:14:08 +0200

    add solution for Unique Morse Code Words ( Arrays )

---
+++ Arrays/UniqueMorseCodeWords/Program.cs
@@ -0,0 +1,37 @@
+﻿// Problem 804. Unique Morse Code Words
+// Link https://leetcode.com/problems/unique-morse-code-words/description/
+// Difficulty: Easy
+using System.Text;
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string[] words = { "gin", "zen", "gig", "msg" };
+    int output = UniqueMorseRepresentations(words);
+    Console.WriteLine(output);
+  }
+  public static int UniqueMorseRepresentations(string[] words)
+  {
+    string[] morse = {
+      ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+      ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+      "...","-","..-","...-",".--","-..-","-.--","--.."
+    };
+
+    HashSet<string> transformations = new HashSet<string>();
+
+    foreach (string word in words)
+    {
+      StringBuilder transformedWord = new StringBuilder();
+
+      foreach (char c in word)
+      {
+        transformedWord.Append(morse[c - 'a']);
+      }
+
+      transformations.Add(transformedWord.ToString());
+    }
+
+    return transformations.Count;
+  }
+}
