a20428582d70a5da5d4a5ecf2d959c7942793589
DimitarMalamski <mitemalamata@gmail.com>
2026-04-09 16:01:57 +0200

    add solution for Shortest Distance to a Character ( Arrays )

---
+++ Arrays/ShortestDistanceToACharacter/Program.cs
@@ -0,0 +1,44 @@
+﻿// Problem 821. Shortest Distance to a Character
+// Link https://leetcode.com/problems/shortest-distance-to-a-character/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "loveleetcode";
+    char c = 'e';
+    int[] output = ShortestToChar(s, c);
+    Console.WriteLine(string.Join(" ", output));
+  }
+  public static int[] ShortestToChar(string s, char c)
+  {
+    int n = s.Length;
+    int[] answer = new int[n];
+
+    int prev = -n;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (s[i] == c)
+      {
+        prev = i;
+      }
+
+      answer[i] = i - prev;
+    }
+
+    prev = 2 * n;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+      if (s[i] == c)
+      {
+        prev = i;
+      }
+
+      answer[i] = Math.Min(answer[i], prev - i);
+    }
+
+    return answer;
+  }
+}
