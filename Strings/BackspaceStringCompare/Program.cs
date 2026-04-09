cf7b9e6c1f9340cc6bdc0059d1cf5b410176951f
DimitarMalamski <mitemalamata@gmail.com>
2026-04-09 16:37:12 +0200

    add solution for Backspace String Compare ( Strings )

---
+++ Strings/BackspaceStringCompare/Program.cs
@@ -0,0 +1,61 @@
+﻿// Problem 844. Backspace String Compare
+// Link https://leetcode.com/problems/backspace-string-compare/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "ab#c", t = "ad#c";
+    bool output = BackspaceCompare(s, t);
+    Console.WriteLine(output);
+  }
+  public static bool BackspaceCompare(string s, string t)
+  {
+    int i = s.Length - 1;
+    int j = t.Length - 1;
+
+    while (i >= 0 || j >= 0)
+    {
+      i = GetNextValidIndex(s, i);
+      j = GetNextValidIndex(t, j);
+
+      if (i >= 0 && j >= 0)
+      {
+        if (s[i] != t[j]) return false;
+      }
+      else if (i >= 0 || j >= 0)
+      {
+        return false;
+      }
+
+      i--;
+      j--;
+    }
+
+    return true;
+  }
+  private static int GetNextValidIndex(string str, int index)
+  {
+    int skip = 0;
+
+    while (index >= 0)
+    {
+      if (str[index] == '#')
+      {
+          skip++;
+          index--;
+      }
+      else if (skip > 0)
+      {
+          skip--;
+          index--;
+      }
+      else
+      {
+          break;
+      }
+    }
+
+    return index;
+  }
+}
