b6d43cfcdda308b37f723566269f29df3e3bc956
DimitarMalamski <mitemalamata@gmail.com>
2026-04-15 18:20:44 +0200

    add solution for Projection Area of 3D Shapes ( Arrays )

---
+++ Arrays/ProjectionAreaOf3DShapes/Program.cs
@@ -0,0 +1,46 @@
+﻿// Problem 883. Projection Area of 3D Shapes
+// Link https://leetcode.com/problems/projection-area-of-3d-shapes/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[][] grid = new int[][]
+    {
+        new int[] {1, 2},
+        new int[] {3, 4}
+    };
+
+    Solution sol = new Solution();
+    int result = sol.ProjectionArea(grid);
+
+    Console.WriteLine(result);
+  }
+  public int ProjectionArea(int[][] grid)
+  {
+    int n = grid.Length;
+    int area = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int rowMax = 0;
+        int colMax = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (grid[i][j] > 0)
+            {
+              area++;
+            }
+
+            rowMax = Math.Max(rowMax, grid[i][j]);
+
+            colMax = Math.Max(colMax, grid[j][i]);
+        }
+
+        area += rowMax + colMax;
+    }
+
+    return area;
+  }
+}
