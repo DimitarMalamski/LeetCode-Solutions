1a2da8aaca87ff56d2aa17c4a51315cb10eed131
DimitarMalamski <mitemalamata@gmail.com>
2026-04-15 18:31:59 +0200

    add solution for Fair Candy Swap ( Arrays )

---
+++ Arrays/FairCandySwap/Program.cs
@@ -0,0 +1,44 @@
+﻿// Problem 888. Fair Candy Swap
+// Link https://leetcode.com/problems/fair-candy-swap/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] aliceSizes = { 1, 1, };
+    int[] bobSizes = { 2, 2 };
+    int[] output = FairCandySwap(aliceSizes, bobSizes);
+    Console.WriteLine(string.Join(", ", output));
+  }
+  public static int[] FairCandySwap(int[] aliceSizes, int[] bobSizes)
+  {
+    int sumA = 0;
+    int sumB = 0;
+
+    foreach (int a in aliceSizes)
+    {
+      sumA += a;
+    }
+
+    foreach (int b in bobSizes)
+    {
+      sumB += b;
+    }
+
+    int diff = (sumB - sumA) / 2;
+
+    HashSet<int> bobSet = new HashSet<int>(bobSizes);
+
+    foreach (int a in aliceSizes)
+    {
+      int b = a + diff;
+
+      if (bobSet.Contains(b))
+      {
+        return new int[] { a, b };
+      }
+    }
+
+    return new int[0];
+  }
+}
