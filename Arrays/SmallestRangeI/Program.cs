0b4970d785a3f2a74662e59d03f0c9b20df3b8a0
DimitarMalamski <mitemalamata@gmail.com>
2026-04-19 13:50:16 +0200

    add solution for Smallest Range I ( Arrays )

---
+++ Arrays/SmallestRangeI/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 908. Smallest Range I
+// Link https://leetcode.com/problems/smallest-range-i/description/
+// Difficulty: Easy
+class Solution {
+  static void Main(string[] args) {
+    int[] nums = { 0, 10 };
+    int k = 2;
+    int output = SmallestRangeI(nums, k);
+    Console.WriteLine(output);
+  }
+  public static int SmallestRangeI(int[] nums, int k) {
+    int min = nums[0];
+    int max = nums[0];
+
+    for (int i = 1; i < nums.Length; i++) {
+      if (nums[i] < min) {
+        min = nums[i];
+      }
+
+      if (nums[i] > max) {
+        max = nums[i];
+      }
+    }
+
+    return Math.Max(0, max - min - 2 * k);
+  }
+}
