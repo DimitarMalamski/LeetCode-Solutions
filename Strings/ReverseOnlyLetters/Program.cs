6a6b058305f3232275b46ed51ee2dbea245140f4
DimitarMalamski <mitemalamata@gmail.com>
2026-04-21 12:24:26 +0200

    add solution for Reverse Only Letters ( Strings )

---
+++ Strings/ReverseOnlyLetters/Program.cs
@@ -0,0 +1,41 @@
+﻿// Problem 917. Reverse Only Letters
+// Link https://leetcode.com/problems/reverse-only-letters/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "a-bC-dEf-ghIj";
+    string output = ReverseOnlyLetters(s);
+    Console.WriteLine(output);
+  }
+  public static string ReverseOnlyLetters(string s)
+  {
+    char[] chars = s.ToCharArray();
+    int left = 0;
+    int right = chars.Length - 1;
+
+    while (left < right)
+    {
+      if (!char.IsLetter(chars[left]))
+      {
+        left++;
+      }
+      else if (!char.IsLetter(chars[right]))
+      {
+        right--;
+      }
+      else
+      {
+        char temp = chars[left];
+        chars[left] = chars[right];
+        chars[right] = temp;
+
+        left++;
+        right--;
+      }
+    }
+
+    return new string(chars);
+  }
+}
