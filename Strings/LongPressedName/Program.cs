bb3b00fff61218f3964dd454740d95b882a71be4
DimitarMalamski <mitemalamata@gmail.com>
2026-05-03 21:16:04 +0200

    add solution for Long Pressed Name ( Strings )

---
+++ Strings/LongPressedName/Program.cs
@@ -0,0 +1,36 @@
+﻿// Problem 925. Long Pressed Name
+// Link https://leetcode.com/problems/long-pressed-name/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string name = "alex", typed = "aaleex";
+    bool output = IsLongPressedName(name, typed);
+    Console.WriteLine(output);
+  }
+  public static bool IsLongPressedName(string name, string typed)
+  {
+    int i = 0;
+    int j = 0;
+
+    while (j < typed.Length)
+    {
+      if (i < name.Length && name[i] == typed[j])
+      {
+        i++;
+        j++;
+      }
+      else if (j > 0 && typed[j] == typed[j - 1])
+      {
+        j++;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    return i == name.Length;
+  }
+}
