e7f3d6252e0f022ea28f4dccdeb21202b6e2d541
DimitarMalamski <mitemalamata@gmail.com>
2026-05-03 21:37:50 +0200

    add solution for Number of Recent Calls ( Stack )

---
+++ Stack/NumberOfRecentCalls/Program.cs
@@ -0,0 +1,36 @@
+﻿// Problem 933. Number of Recent Calls
+// Link https://leetcode.com/problems/number-of-recent-calls/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    RecentCounter recentCounter = new RecentCounter();
+
+    Console.WriteLine(recentCounter.Ping(1));
+    Console.WriteLine(recentCounter.Ping(100));
+    Console.WriteLine(recentCounter.Ping(3001));
+    Console.WriteLine(recentCounter.Ping(3002));
+  }
+}
+class RecentCounter
+{
+  private Queue<int> requests;
+
+  public RecentCounter()
+  {
+    requests = new Queue<int>();
+  }
+
+  public int Ping(int t)
+  {
+    requests.Enqueue(t);
+
+    while (requests.Peek() < t - 3000)
+    {
+      requests.Dequeue();
+    }
+
+    return requests.Count;
+  } 
+} 
