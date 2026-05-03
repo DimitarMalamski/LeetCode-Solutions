c58248d947f1e000f45c3e46b7dbd86eb9744333
DimitarMalamski <mitemalamata@gmail.com>
2026-05-03 22:08:59 +0200

    add solution for N-Repeated Element in Size 2N Array ( Arrays )

---
+++ Arrays/N-RepeatedElementInSize2NArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// Problem 961. N-Repeated Element in Size 2N Array
+// Link https://leetcode.com/problems/n-repeated-element-in-size-2n-array/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] nums = { 1, 2, 3, 3 };
+    int output = RepeatedNTimes(nums);
+    Console.WriteLine(output);
+  }
+  public static int RepeatedNTimes(int[] nums)
+  {
+    HashSet<int> seen = new HashSet<int>();
+
+    foreach (int num in nums)
+    {
+      if (seen.Contains(num))
+      {
+        return num;
+      }
+
+      seen.Add(num);
+    }
+
+    return -1;
+  } 
+}
