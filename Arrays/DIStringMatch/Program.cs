39a9297fa316af4b60fa2ea5d31045174be71d04
DimitarMalamski <mitemalamata@gmail.com>
2026-05-03 21:54:10 +0200

    add solution for DI String Match ( Arrays )

---
+++ Arrays/DIStringMatch/Program.cs
@@ -0,0 +1,38 @@
+﻿// Problem 942. DI String Match
+// Link https://leetcode.com/problems/di-string-match/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string s = "IDID";
+    int[] output = DiStringMatch(s);
+    Console.WriteLine(string.Join(", ", output));
+  }
+  public static int[] DiStringMatch(string s)
+  {
+    int n = s.Length;
+    int[] result = new int[n + 1];
+
+    int low = 0;
+    int high = n;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (s[i] == 'I')
+      {
+        result[i] = low;
+        low++;
+      }
+      else
+      {
+        result[i] = high;
+        high--;
+      }
+    }
+
+    result[n] = low;
+
+    return result;
+  }
+}
