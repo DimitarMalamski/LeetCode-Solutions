1773a2b8e00b3c8898229e7808a0a9759c2d1321
DimitarMalamski <mitemalamata@gmail.com>
2026-05-12 12:13:52 +0300

    add solution for Complement of Base 10 Integer ( Bit Manipulation )

---
+++ Bit Manipulation/ComplementOfBase10Integer/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 1009. Complement of Base 10 Integer
+// Link https://leetcode.com/problems/complement-of-base-10-integer/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int n = 5;
+    int output = BitwiseComplement(n);
+    Console.WriteLine(output);
+  }
+  public static int BitwiseComplement(int n)
+  {
+    if (n == 0) return 1;
+
+    int mask = 0;
+    int temp = n;
+
+    while (temp > 0)
+    {
+      mask = (mask << 1) | 1;
+      temp >>= 1;
+    }
+
+    return mask ^ n;
+  }
+}
