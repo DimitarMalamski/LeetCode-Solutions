6c210dafbd8e277eb0d0ce82bfd23bb5308db572
DimitarMalamski <mitemalamata@gmail.com>
2026-05-17 17:47:47 +0300

    add solution for Divisor Game ( Math )

---
+++ Math/DivisorGame/Program.cs
@@ -0,0 +1,16 @@
+﻿// Problem 1025. Divisor Game
+// Link https://leetcode.com/problems/divisor-game/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int n = 2;
+    bool output = DivisorGame(n);
+    Console.WriteLine(output);
+  }
+  public static bool DivisorGame(int n)
+  {
+    return n % 2 == 0;
+  }
+}
