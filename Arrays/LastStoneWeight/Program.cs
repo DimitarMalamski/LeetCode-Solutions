083531de3514e24457990a4bef7b3358c2834fdc
DimitarMalamski <mitemalamata@gmail.com>
2026-05-19 21:41:44 +0200

    add solution for Last Stone Weight ( Arrays )

---
+++ Arrays/LastStoneWeight/Program.cs
@@ -0,0 +1,34 @@
+﻿// Problem 1046. Last Stone Weight
+// Link https://leetcode.com/problems/last-stone-weight/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] stones = { 2, 7, 4, 1, 8, 1 };
+    int output = LastStoneWeight(stones);
+    Console.WriteLine(output);
+  }
+  public static int LastStoneWeight(int[] stones)
+  {
+    List<int> stoneList = new List<int>(stones);
+
+    while (stoneList.Count > 1)
+    {
+      stoneList.Sort();
+
+      int y = stoneList[stoneList.Count - 1];
+      int x = stoneList[stoneList.Count - 2];
+
+      stoneList.RemoveAt(stoneList.Count - 1);
+      stoneList.RemoveAt(stoneList.Count - 1);
+
+      if (y != x)
+      {
+        stoneList.Add(y - x);
+      }
+    }
+
+    return stoneList.Count == 0 ? 0 : stoneList[0];
+  }
+}
