0a1d1f3970fd5dbcf0a15a8a2ac7be339b1340dc
DimitarMalamski <mitemalamata@gmail.com>
2026-05-22 10:56:37 +0200

    add solution for Duplicate Zeros ( Arrays )

---
+++ Arrays/DuplicateZeros/Program.cs
@@ -0,0 +1,41 @@
+﻿// Problem 1089. Duplicate Zeros
+// Link https://leetcode.com/problems/duplicate-zeros/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] arr = { 1, 0, 2, 3, 0, 4, 5, 0 };
+    DuplicateZeros(arr);
+    Console.WriteLine(string.Join(",", arr));
+  }
+  public static void DuplicateZeros(int[] arr)
+  {
+    int[] copy = (int[])arr.Clone();
+
+    int read = 0;
+    int write = 0;
+
+    while (write < arr.Length && read < copy.Length)
+    {
+      if (copy[read] == 0)
+      {
+        arr[write] = 0;
+        write++;
+
+        if (write < arr.Length)
+        {
+          arr[write] = 0;
+          write++;
+        }
+      }
+      else
+      {
+        arr[write] = copy[read];
+        write++;
+      }
+
+      read++;
+    }
+  }
+}
