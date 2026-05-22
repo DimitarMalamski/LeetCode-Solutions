7e3247ed8149ba3cf92dd5c5e1a89b25cb8c3b59
DimitarMalamski <mitemalamata@gmail.com>
2026-05-22 10:51:03 +0200

    add solution for Occurrences After Bigram ( Strings )

---
+++ Strings/OccurrencesAfterBigram/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 1078. Occurrences After Bigram
+// Link https://leetcode.com/problems/occurrences-after-bigram/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string text = "alice is a good girl she is a good student", first = "a", second = "good";
+    string[] output = FindOcurrences(text, first, second);
+    Console.WriteLine(string.Join(",", output));
+  }
+  public static string[] FindOcurrences(string text, string first, string second)
+  {
+    string[] words = text.Split(' ');
+    List<string> result = new List<string>();
+
+    for (int i = 0; i < words.Length - 2; i++)
+    {
+      if (words[i] == first && words[i + 1] == second)
+      {
+        result.Add(words[i + 2]);
+      }
+    }
+
+    return result.ToArray();
+  }
+}
