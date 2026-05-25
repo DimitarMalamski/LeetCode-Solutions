d0f63136e316bf2c24054f70ea07d5dd4757c768
DimitarMalamski <mitemalamata@gmail.com>
2026-05-25 09:55:06 +0200

    add solution for Number of Equivalent Domino Pairs ( Arrays )

---
+++ Arrays/NumberOfEquivalentDominoPairs/Program.cs
@@ -0,0 +1,40 @@
+﻿// Problem 1128. Number of Equivalent Domino Pairs
+// Link https://leetcode.com/problems/number-of-equivalent-domino-pairs/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[][] dominoes = {
+      new int[] { 1,2},
+      new int[] { 2,1},
+      new int[] { 3,4},
+      new int[] { 5,6},
+    };
+
+    int output = NumEquivDominoPairs(dominoes);
+    Console.WriteLine(output);
+  }
+  public static int NumEquivDominoPairs(int[][] dominoes)
+  {
+    int[] count = new int[100];
+    int result = 0;
+
+    foreach (int[] domino in dominoes)
+    {
+      int a = domino[0];
+      int b = domino[1];
+
+      int min = Math.Min(a, b);
+      int max = Math.Max(a, b);
+
+      int key = min * 10 + max;
+
+      result += count[key];
+
+      count[key]++;
+    }
+
+    return result;
+  }
+}
