2bc1976d8b91f6b45c2170c9c48aeba24c187ce6
DimitarMalamski <mitemalamata@gmail.com>
2026-05-29 14:48:35 +0200

    add solution for Find Words That Can Be Formed by Characters ( Arrays )

---
+++ Arrays/FindWordsThatCanBeFormedByCharacters/Program.cs
@@ -0,0 +1,52 @@
+﻿// Problem 1160. Find Words That Can Be Formed by Characters
+// Link https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    string[] words = { "cat", "bt", "hat", "tree" };
+    string chars = "atach";
+    int output = CountCharacters(words, chars);
+    Console.WriteLine(output);
+  }
+  public static int CountCharacters(string[] words, string chars)
+  {
+    int[] available = new int[26];
+
+    foreach (char c in chars)
+    {
+      available[c - 'a']++;
+    }
+
+    int totalLength = 0;
+
+    foreach (string word in words)
+    {
+      int[] needed = new int[26];
+
+      foreach (char c in word)
+      {
+        needed[c - 'a']++;
+      }
+
+      bool canForm = true;
+
+      for (int i = 0; i < 26; i++)
+      {
+        if (needed[i] > available[i])
+        {
+          canForm = false;
+          break;
+        }
+      }
+
+      if (canForm)
+      {
+        totalLength += word.Length;
+      }
+    }
+
+    return totalLength;
+  } 
+}
