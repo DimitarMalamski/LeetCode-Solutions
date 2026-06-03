5caa5d60d4cb5b11ca6b7f2b98c512d0be16934d
DimitarMalamski <mitemalamata@gmail.com>
2026-06-03 14:30:10 +0200

    add solution for Minimum Cost to Move Chips to The Same Position ( Arrays )

---
+++ Arrays/MinimumCostToMoveChipsToTheSamePosition/Program.cs
@@ -0,0 +1,31 @@
+﻿// Problem 1217. Minimum Cost to Move Chips to The Same Position
+// Link https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] position = { 1, 2, 3 };
+    int output = MinCostToMoveChips(position);
+    Console.WriteLine(output);
+  }
+  public static int MinCostToMoveChips(int[] position)
+  {
+    int odd = 0;
+    int even = 0;
+
+    foreach (int pos in position)
+    {
+      if (pos % 2 == 0)
+      {
+        even++;
+      }
+      else
+      {
+        odd++;
+      }
+    }
+
+    return Math.Min(odd, even);
+  }
+}
