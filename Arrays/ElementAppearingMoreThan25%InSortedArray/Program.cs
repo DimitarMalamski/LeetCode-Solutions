3539cb6688ad37c9d08b738c0185834ed63824d0
DimitarMalamski <mitemalamata@gmail.com>
2026-06-11 13:00:26 +0200

    add solution for Element Appearing More Than 25% In Sorted Array ( Arrays )

---
+++ Arrays/ElementAppearingMoreThan25%InSortedArray/Program.cs
@@ -0,0 +1,33 @@
+﻿// Problem 1287. Element Appearing More Than 25% In Sorted Array
+// Link https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/
+// Difficulty: Easy
+class Solution
+{
+  static void Main(string[] args)
+  {
+    int[] arr = { 1, 2, 2, 6, 6, 6, 6, 7, 10 };
+    int output = FindSpecialInteger(arr);
+    Console.WriteLine(output);
+  }
+  public static int FindSpecialInteger(int[] arr)
+  {
+    int count = 1;
+    int limit = arr.Length / 4;
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+      if (arr[i] == arr[i - 1])
+      {
+        count++;
+      }
+      else
+      {
+        count = 1;
+      }
+
+      if (count > limit) return arr[i];
+    }
+
+    return arr[0];
+  }
+}
